60cb43072496b8022cfa4045d1df2d583b0a8679
TNMYDNA <tnmydna@gmail.com>
2026-06-09 17:15:53 +0530

    Improved auto replies and history display

--- view_data.py
+++ view_data.py
@@ -1,14 +1,16 @@
 import sqlite3
 
 conn = sqlite3.connect("youtube.db")
-
 cursor = conn.cursor()
 
 cursor.execute("SELECT * FROM comments")
-
 rows = cursor.fetchall()
 
 for row in rows:
-    print(row)
+    print("ID:", row[0])
+    print("Comment:", row[1])
+    print("Reply:", row[2])
+    print("Video ID:", row[3])
+    print("--------------------------------")
 
 conn.close()
