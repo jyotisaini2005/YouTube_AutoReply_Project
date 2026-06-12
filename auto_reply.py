6d3931eb1e287572eff4016d61740f5c89d6b421
TNMYDNA <tnmydna@gmail.com>
2026-06-12 19:57:02 +0530

    Fixed auto_reply for Streamlit app

--- auto_reply.py
+++ auto_reply.py
@@ -4,7 +4,6 @@ with open("keywords.json", "r", encoding="utf-8") as file:
     keywords = json.load(file)
 
 def get_reply(comment):
-
     comment = comment.lower()
 
     for keyword, reply in keywords.items():
@@ -12,3 +11,7 @@ def get_reply(comment):
             return reply
 
     return "Thank you for your comment 😊"
+
+if __name__ == "__main__":
+    comment = input("Enter Comment: ")
+    print("Auto Reply:", get_reply(comment))
